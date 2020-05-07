18ab434abff46c82b3b31dca9f7f2493ccf99998
Zach Nicholson <raiste.doctow@gmail.com>
2020-05-07 15:16:10 -0500

    bestmonth fixed, bestagent finished

--- main.js
+++ main.js
@@ -3,26 +3,12 @@ import data from './data.js'
 let sales2017 = data.cars.filter(c => c.purchase_date.includes('2017'))
 
 // gross profit 2017
-
 let profit = 0
 for (let i = 0; i < sales2017.length; i++) {
     profit += sales2017[i].gross_profit
 }
 
-// month in which most cars were sold
-let bestMonth = 0;
-const monthlySales = sales2017.flatMap(car => car.purchase_date).map(date => date.slice(5, 7)).reduce((a, b) => {
-    a[b] = (a[b] || 0) + 1; 
-    return a
-}, {})
-for (let i = 0; i < Object.keys(monthlySales).length; i++) {
-    if (i === 0 || Object.keys(monthlySales)[i - 1] < Object.keys(monthlySales)[i]) {
-        bestMonth = Object.keys(monthlySales)[i];
-    }
-}
-
 // counts how many of each repeated value is in an array, then creates an object with { value: timesRepeated } for each value
-
 const dupeCounter = (array) => {
     let counts = {}
     array.forEach(i => {
@@ -31,20 +17,13 @@ const dupeCounter = (array) => {
     return counts;
 }
 
-// returns the key most repeated item in an object as a string
+// returns the key with most repeated items in an object as a string
+const findMostRepeatedItem = object => Object.entries(object).sort((a, b) => a[1] - b[1]).pop()[0]
 
-const findMostRepeatedItem = (object, emptyVariable) => {
-    for (let i = 0; i < Object.keys(object).length; i++) {
-        if (i === 0 || Object.keys(object)[i - 1] < Object.keys(object)[i]) {
-            emptyVariable = Object.keys(object)[i];
-        }
-    }    
-}
+// month in which most cars were sold
+const monthlySales = dupeCounter(sales2017.flatMap(car => car.purchase_date).map(date => date.slice(5, 7)))
+const bestMonth = findMostRepeatedItem(monthlySales);
 
 // agent who sold most cars
-
-let bestAgent = ''
-
 const agents = dupeCounter(sales2017.map(car => car.sales_agent.first_name + ' ' + car.sales_agent.last_name))
-
-console.log(findMostRepeatedItem(agents, bestAgent));
+const bestAgent = findMostRepeatedItem(agents);
